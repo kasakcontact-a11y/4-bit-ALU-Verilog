88cb7192506188f29ed72f4f8134605932ee55cf
kasakcontact-a11y <kasak.contact@gmail.com>
2026-03-27 02:13:18 -0400

    Update alu_tb.v

--- alu_tb.v
+++ alu_tb.v
@@ -5,24 +5,28 @@ reg [2:0] sel;
 wire [3:0] result;
 wire carry;
 
-alu uut(A, B, sel, result, carry);
+alu uut (
+    .A(A),
+    .B(B),
+    .sel(sel),
+    .result(result),
+    .carry(carry)
+);
 
 initial begin
+    $dumpfile("alu.vcd");
+    $dumpvars(0, alu_tb);
 
-  $dumpfile("alu.vcd");
-  $dumpvars(0, alu_tb);
+    A=4'b0101; B=4'b0011; sel=3'b000;
+    #10 sel=3'b001;
+    #10 sel=3'b010;
+    #10 sel=3'b011;
+    #10 sel=3'b100;
+    #10 sel=3'b101;
+    #10 sel=3'b110;
+    #10 sel=3'b111;
 
-  A=4'b0101; B=4'b0011; sel=3'b000;
-  #10 sel=3'b001;
-  #10 sel=3'b010;
-  #10 sel=3'b011;
-  #10 sel=3'b100;
-  #10 sel=3'b101;
-  #10 sel=3'b110;
-  #10 sel=3'b111;
-
-  #10 $finish;
-
+    #10 $finish;
 end
 
 endmodule
